7f5ce05121e820653231ff605bdeeff1bc727922
Vinícius Rocha <vinicius.a.rocha8@aluno.senai.br>
2025-08-07 13:58:26 -0300

    fix: fix image

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,15 +7,14 @@ return (
             <img src={imagem} alt={nome} style={{maxWidth: '100%', maxHeight: '100%'}} />
         </div> 
         <div>
-            <h4 className={styles.productTitle}>{nome ? nome : "Nome não informado"}</h4>
+            <h4 className={styles.productName}>{nome ? nome : "Nome não informado"}</h4>
             <p className={styles.productPrice}>{preco ? `Preço: R$ ${preco}` : "Preço não informado"}</p>
             <p className={styles.productCategory}>{categoria ? `Categoria: ${categoria}` : "Categoria não informada"}</p>
             <p className={styles.productDescription}>{descricao ? descricao : "Descrição não informada"}</p>
-            
             <p className={styles.productRating}>
                 {avaliacao ? (
                     <>
-                        Avaliação: <span className={styles.stars}>{'★'.repeat(avaliacao)}</span>
+                       Avaliação: <span className={styles.stars}>{'★'.repeat(avaliacao)}</span>
                     </>
                 ) : "Avaliação não informada"}
             </p>
